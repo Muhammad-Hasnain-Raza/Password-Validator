4b559e59d984d56bcc0219f4f2a57a6d714d1256
Muhammad Hasnain Raza <hassnainrazam009@gmail.com>
2024-07-23 00:35:46 +0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-let 
+let in_visible = false
+
+function see(){
+    var input = document.getElementById("password")
+    var see = document.getElementById("see")
+
+    if(in_visible){
+        input.type = 'password'
+        is_visible = false
+    }else{
+        input.type = 'text'
+        is_visible = true
+    }
+}
+
+function check(){
+    var input = document.getElementById("password").value
+
+    if(input.match(/[0-9]/i)){
+        document.getElementById("check0").style.color = "green";
+    }else{
+        document.getElementById("check0").style.color = "red";
+    }
+
+    
+    if(input.match('')){
+        document.getElementById("check1").style.color = "green";
+    }else{
+        document.getElementById("check1").style.color = "red";
+    }
+
+    if(input.length >= 5){
+        document.getElementById("check2").style.color = "green";
+    }else{
+        document.getElementById("check2").style.color = "red";
+    }
+
+    if(input.length <= 15){
+        document.getElementById("check3").style.color = "green";
+    }else{
+        document.getElementById("check3").style.color = "red";
+    }
+
+    if(input.match(/[^A-Za-z0-9-'']/i)){
+        document.getElementById("check4").style.color = "green";
+    }else{
+        document.getElementById("check4").style.color = "red";
+    }
+}
